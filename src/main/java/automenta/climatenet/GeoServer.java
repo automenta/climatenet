f947294ad43c572d26077695550c772e6fe7bf24
SeH <1s1e1h1@gmail.com>
2014-12-18 04:21:45 -0500

    updates

---
+++ src/main/java/automenta/climatenet/GeoServer.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package automenta.climatenet;
+
+/**
+ *
+ * @author me
+ */
+public class GeoServer {
+    //http://stackoverflow.com/questions/25187937/loading-geojson-layers-from-geoserver-to-leaflet-map-based-on-the-current-boundi
+    //http://www.gaiaresources.com.au/json-with-geoserver-and-leaflet/
+    //http://docs.geoserver.org/latest/en/user/production/container.html
+    //http://docs.geoserver.org/latest/en/user/production/config.html
+    //
+}
--- src/main/java/automenta/climatenet/ImportKML.java
+++ src/main/java/automenta/climatenet/ImportKML.java
@@ -10,30 +10,114 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.net.URI;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
+import java.util.zip.ZipOutputStream;
+import org.opensextant.giscore.DocumentType;
+import org.opensextant.giscore.GISFactory;
+import org.opensextant.giscore.events.ContainerStart;
+import org.opensextant.giscore.events.DocumentStart;
+import org.opensextant.giscore.events.Feature;
 import org.opensextant.giscore.events.IGISObject;
+import org.opensextant.giscore.events.Schema;
+import org.opensextant.giscore.events.SimpleField;
 import org.opensextant.giscore.input.kml.KmlReader;
 import org.opensextant.giscore.input.kml.UrlRef;
+import org.opensextant.giscore.output.IGISOutputStream;
+import org.opensextant.giscore.output.kml.KmlOutputStream;
 
 /**
- *
+ * @see https://github.com/OpenSextant/giscore/wiki
  * @author me
  */
 public class ImportKML {
 
-    public static void importKMLLayer(String urlString) throws Exception {
+    String basePath = "data";
+    String layersFile = "src/main/java/automenta/climatenet/cvlayers.json";
+    
+    public void transformKML(String layer, String urlString, boolean esri, boolean kml) throws Exception {
         URL url = new URL(urlString);
         
         KmlReader reader = new KmlReader(url);
-// read all features from KML
+        reader.setRewriteStyleUrls(true);
+        
+        
+        
+        
+        File temp = Files.createTempDirectory("kml" + layer).toFile();
+        
+        String p = basePath + "/" + layer;
+        
+        //Files.deleteIfExists(Paths.get(p));
+        if (!Files.exists(Paths.get(p)))
+            Files.createDirectory(Paths.get(p));
+        
+        
+        IGISOutputStream shpos = null;
+        ZipOutputStream esriOut = null;
+        
+        if (esri) {
+            String outputFile = p + "/" + layer + ".shape.zip";
+            if (Files.exists(Paths.get(outputFile)))
+                Files.delete(Paths.get(outputFile));
+            esriOut = new ZipOutputStream(new FileOutputStream(outputFile));
+
+
+            shpos = GISFactory.getOutputStream(DocumentType.Shapefile, esriOut, temp);
+            System.out.println("  ESRI Shapefile: Output file: " + outputFile);
+            System.out.println("  ESRI Shapefile: Temporary folder: " + temp);
+        
+            Schema schema = new Schema(new URI("urn:test"));
+            SimpleField id = new SimpleField(layer);
+            id.setLength(10);
+            schema.put(id);
+            DocumentStart ds = new DocumentStart(DocumentType.Shapefile);
+            shpos.write(ds);
+            ContainerStart cs = new ContainerStart("Folder");
+            cs.setName(layer);
+            shpos.write(cs);
+            shpos.write(schema);
+        }
+        
+        KmlOutputStream kos = null;
+        if (kml) {
+            String outputFile = p + "/" + layer + ".kml";
+            
+            FileOutputStream out = new FileOutputStream(new File(outputFile));
+            kos = new KmlOutputStream(out, "UTF-8");
+            
+            //TODO
+            //tee out to a pipe that executes:
+            //  {cat cvr01.kml |} ogr2ogr -F GeoJSON cvr01.json /vsistdin/
+        }
+        
+        
+        
+                
+	
+        
+        
+        
         
         for (IGISObject gisObj; (gisObj = reader.read()) != null;) {
   // do something with the gis object; e.g. check for placemark, NetworkLink, etc.
-            System.out.println(gisObj);
+            if (gisObj instanceof Feature) {
+                Feature f = (Feature)gisObj;                
+                
+                //System.out.println(f.getElements());
+                //System.out.println(f.getFields() + " " + schema.getFields());
+                
+                if (esri)
+                    shpos.write(f);  
+                if (kml)
+                    kos.write(f);
+                
+                //System.out.println(gisObj);
+            }
         }
 // get list of network links that were retrieved from step above
         
@@ -63,12 +147,27 @@ public class ImportKML {
                     });
 
         }
+        
+        if (esri) {
+            shpos.close();
+
+            esriOut.flush();
+            esriOut.close();	
+        }
+        if (kml) {
+            kos.close();
+        }
+        
+        System.out.println("finished");
+        System.exit(0);
     }
-    
 
-    public static void main(String[] args) throws Exception {
+    public ImportKML() throws Exception {
 
-        URI uri = new File("src/main/java/automenta/climatenet/cvlayers.json").toURI();
+        if (!Files.exists(Paths.get(basePath)))
+            Files.createDirectory(Paths.get(basePath));
+        
+        URI uri = new File(layersFile).toURI();
 
         byte[] encoded = Files.readAllBytes(Paths.get(uri));
 
@@ -85,12 +184,17 @@ public class ImportKML {
             } else if (!x.isObject() && !x.has("layer")) {
                 System.err.println("Unrecognized item: " + x);
             } else {
-
+                String layer = x.get("layer").textValue();
                 String url = x.get("kml").textValue();
                 System.out.println(currentSection + " " + x);
-                importKMLLayer(url);
+                transformKML(layer, url, false, true);
             }
         }
+    }
+    
 
+    
+    public static void main(String[] args) throws Exception {
+        new ImportKML();
     }
 }
